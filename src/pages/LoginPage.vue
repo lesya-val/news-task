ef66e7fa7489ab941fffd74de898eb6e5a70c8a2
lesya-val <you@example.com>
2023-07-08 12:13:16 +0600

    feat: make login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="LoginPage">
-		<h1>Вход</h1>
-		<form class="LoginPage__Form">
-			<label for="login">Логин:</label>
-			<input type="text" id="login" class="LoginPage__Wrapper-Login">
-			<label for="password">Пароль:</label>
-			<input type="text" id="password" class="LoginPage__Wrapper-Password">
-		</form>
-		<button class="LoginPage__Button">Войти</button>
+		<headerSection/>
+		<div class="LoginPage__Wrapper">
+			<h1 class="LoginPage__Title">Авторизация</h1>
+			<loginForm/>
+		</div>
 	</div>
+
 </template>
 
 <script>
 
+	import headerSection from '@/components/HeaderSection.vue';
+	import loginForm from '@/components/LoginForm.vue'
+
 	export default {
 		name: 'LoginPage',
 		components: {
-			
+			headerSection,
+			loginForm,
 		},
 		data () {
 			return {
@@ -28,12 +30,17 @@
 
 <style lang="scss">
 	.LoginPage {
-		&__Wrapper-Login {
-			border: 1px solid black;
+		&__Wrapper {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			row-gap: 30px;
+			margin-top: 100px;
 		}
 
-		&__Wrapper-Password {
-			border: 1px solid black;
+		&__Title {
+			font-size: 25px;
 		}
 	}
 </style>
